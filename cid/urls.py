8147b45395d078d141955df7e5430da50b705774
david <david.santucci@cloudacademy.com>
2017-11-05 11:28:10 +0100

    Add mocked views and urls

--- cid/urls.py
+++ cid/urls.py
@@ -1,12 +1,13 @@
 from rest_framework import routers
 
-from cid.views import VehicleViewSet, ProfileViewSet, AccidentViewSet
+from cid.views import VehicleViewSet, ProfileViewSet, AccidentViewSet, AniaMockViewSet, AciMockViewSet, AroundMeMockViewSet
 
-urlpatterns = [
-    # url(r'^test-url$', test_url, name='test_url'),
-]
+urlpatterns = []
 
 router = routers.DefaultRouter()
 router.register(r'vehicles', VehicleViewSet)
 router.register(r'profiles', ProfileViewSet)
 router.register(r'accidents', AccidentViewSet)
+router.register(r'ania_api/(?P<targa>\d+)', AniaMockViewSet, 'ania_api')
+router.register(r'aci_api/(?P<patente>\d+)', AciMockViewSet, 'aci_api')
+router.register(r'trova_carroattrezzi/', AroundMeMockViewSet, 'trova_carroattrezzi')
--- cid/views.py
+++ cid/views.py
@@ -1,5 +1,10 @@
+import json
+
+import requests
 from rest_framework import response, schemas, viewsets
+from rest_framework import status
 from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.response import Response
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
 
 from .serializers import *
@@ -25,3 +30,26 @@ class ProfileViewSet(viewsets.ModelViewSet):
 class AccidentViewSet(viewsets.ModelViewSet):
     queryset = Accident.objects.all()
     serializer_class = AccidentSerializer
+
+
+class AniaMockViewSet(viewsets.ViewSet):
+    def list(self, request, *args, **kwargs):
+        targa = self.kwargs.get('targa', '')
+        return Response({'data': targa}, status=status.HTTP_200_OK)
+
+
+class AciMockViewSet(viewsets.ViewSet):
+    def list(self, request, *args, **kwargs):
+        patente = self.kwargs.get('patente', '')
+        return Response({'data': patente}, status=status.HTTP_200_OK)
+
+
+class AroundMeMockViewSet(viewsets.ViewSet):
+    def list(self, request, *args, **kwargs):
+        url = "https://pp.api.helab.nuvolaitaliana.it/si/contacts/v2/locations?companyAteco=52.21.6"
+        headers = {'Keyid': '4d019814-f574-4d72-9e58-5510c60afc5b'}
+        res = requests.get(url, headers=headers)
+        if res.status_code == 200:
+            return Response(json.loads(res.content), status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'error'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
